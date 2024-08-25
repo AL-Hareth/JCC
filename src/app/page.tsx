b81abdcdd043e0f564b2195983106b15f163b23f
jordanusmlementors <alharethturab@gmail.com>
2024-08-25 15:17:31 +0300

    changed some colors inside the terminal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import Customers from "./(components)/Customers";
 import FAQ from "./(components)/FAQ";
 import Hero from "./(components)/Hero";
-import Navbar from "./(components)/Navbar";
-import Footer from "./(components)/Footer";
 
 export default function Home() {
   return (
