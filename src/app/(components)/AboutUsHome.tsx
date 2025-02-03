0d0a04ec5975733ae641a91caae20960119a9195
AL-Hareth <alharethturab@gmail.com>
2025-02-04 02:19:51 +0300

    fix: changed team name to yellow in about section

--- src/app/(components)/AboutUsHome.tsx
+++ src/app/(components)/AboutUsHome.tsx
@@ -3,7 +3,7 @@ export default function AboutUsHome() {
     <div className="text-center mb-12">
       <h1 className="text-5xl font-bold mb-6">About us</h1>
       <p className={`w-2/3 mx-auto text-2xl leading-10`}>
-        We are the Jordan Cyber Club team, striving to raise awareness of cybersecurity and develop youth skills in Jordan. We aspire to provide education and training in the field of cybersecurity, and to be a center for innovation and research in this field, by providing high-quality educational and training programs that meet the needs and aspirations of the labor market and society.
+        We are the <span className="text-yellow-500">Jordan Cyber Club</span> team, striving to raise awareness of cybersecurity and develop youth skills in Jordan. We aspire to provide education and training in the field of cybersecurity, and to be a center for innovation and research in this field, by providing high-quality educational and training programs that meet the needs and aspirations of the labor market and society.
       </p>
     </div>
   );
